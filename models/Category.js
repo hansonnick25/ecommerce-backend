cb452abe1d9a184cdc5737bb78749cecec69605b
Nick Hanson <hansonnick25@gmail.com>
2023-09-12 14:27:18 -0500

    Updating field structure in models

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,24 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize')
 
-const sequelize = require('../config/connection.js');
+const sequelize = require('../config/connection.js')
 
 class Category extends Model {}
 
 Category.init(
   {
     // define columns
+    // id INT NOT NULL AUTO_INCREMENT
+    // category_name VARCHAR NOT NULL
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
@@ -15,6 +27,6 @@ Category.init(
     underscored: true,
     modelName: 'category',
   }
-);
+)
 
-module.exports = Category;
+module.exports = Category
