152bfe6453ba40ac42896aa6535fb94aeed9fc6f
Nick Hanson <hansonnick25@gmail.com>
2023-09-16 04:52:22 -0500

    Removing comments

--- models/Category.js
+++ models/Category.js
@@ -7,8 +7,6 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
-    // id INT NOT NULL AUTO_INCREMENT
-    // category_name VARCHAR NOT NULL
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
